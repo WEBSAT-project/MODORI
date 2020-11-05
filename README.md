7ba521f1e1e0002b11d5d52100e5ba275d61c9a8
Hong-JunHyeok <edb1631@naver.com>
2020-11-05 11:12:39 +0900

    SET

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # 2020 나르샤
 
 ## Project Modori
+
+## 사용한 라이브러리s
+
+-   styled-component
+-   react-markdown-editor-lite
+-   react-icons
--- src/index.js
+++ src/index.js
@@ -11,3 +11,11 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById("root")
 );
+
+// 분류방법 : CSS, JS 그리고 테스트 파일을 기능이나 라우트로 분류된 폴더에 같이 두는 방법입니다
+// css : styles folder
+// components : components folder
+// container : container folder
+
+// 컴포넌트와 컨테이너의 설명 (컨테이너  >  컴포넌트 )
+// 기능 & 로직같은 것들을 컨테이너에서 구현하고 컴포넌트는 View관련 작업들을 하는곳
