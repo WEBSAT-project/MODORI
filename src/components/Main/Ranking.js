2f25583d2f201a253a2c1c5a6bd7cefc11449edd
toswm128 <chominsu10@gmail.com>
2020-11-17 00:38:47 +0900

    포스트 그리드 생성
    ㅠㅠ 심장 떨어지는 줄...

--- src/components/Main/Ranking.js
+++ src/components/Main/Ranking.js
@@ -4,12 +4,30 @@ import MainHeader from "./MainHeader";
 
 const RankingDiv = styled.div``;
 
+const Stairs = styled.div`
+    display:gird;
+    grid-template-columns:1vh 1vh 1vh;
+    grid-template-rows:1vh 1vh 1vh;
+    grid-template-areas:
+    ". border ."
+    "border border ."
+    "border border border";
+`;
+
+const Border = styled.div`
+    border: 0.1rem solid black;
+    grid-area:border;
+`;
+
 const Ranking = ({ rankList }) => {
     console.log(rankList);
     return (
         <RankingDiv>
             <MainHeader />
-            {rankList}
+            <Stairs>
+                <Border />
+                //{rankList}
+            </Stairs>
         </RankingDiv>
     );
 };
