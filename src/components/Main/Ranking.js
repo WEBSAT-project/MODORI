7bb6caa4b76e0020fe609acffdd897092e228f37
Hong-JunHyeok <edb1631@naver.com>
2020-11-16 13:40:40 +0900

    [ADD]

---
+++ src/components/Main/Ranking.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import MainHeader from "./MainHeader";
+
+const RankingDiv = styled.div``;
+
+const Ranking = () => {
+    return <MainHeader />;
+};
+
+export default Ranking;
